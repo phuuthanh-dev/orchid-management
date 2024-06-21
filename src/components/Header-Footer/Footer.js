383354dcc58607afedb934f632feb80423673bb5
Phung Huu Thanh <102614541+phuuthanh2003@users.noreply.github.com>
2024-06-21 16:49:41 +0700

    fix footer

--- src/components/Header-Footer/Footer.js
+++ src/components/Header-Footer/Footer.js
@@ -1,13 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer>
-      <p>
-        Author: Phung Huu Thanh
-        <br />
-        <a href="mailto:hege@example.com">phuuthanh2003@gmail.com</a>
-      </p>
+    <footer class="py-3 my-4 mb-0" style={{ background: "#333333" }}>
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><Link to={"/"} class="nav-link px-2" style={{ color: "white" }}>Home</Link></li>
+        <li class="nav-item"><Link to={"/contact"} class="nav-link px-2" style={{ color: "white" }}>Contact</Link></li>
+        <li class="nav-item"><Link to={""} class="nav-link px-2" style={{ color: "white" }}>About</Link></li>
+      </ul>
+      <p class="text-center" style={{ color: "white" }}>© 2024 Company, Inc</p>
     </footer>
   );
 }
